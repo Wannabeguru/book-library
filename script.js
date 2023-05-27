3ac535f90d89364575d80191e8d620adfed8bf56
Ophir Bentov <wannabegurux@gmail.com>
2023-05-26 18:36:01 -0700

    added creation of bookcards, and various improvements.

--- script.js
+++ script.js
@@ -1,35 +1,68 @@
 let open = document.getElementById('addBook');
 let modalContainer = document.getElementById('modalContainer');
 let closeBtn = document.getElementById('close-outline')
+let library = document.getElementById('libraryContainer');
 const myForm = document.getElementById('myForm');
 const submitBtn = myForm.elements['submit'];
+
 const myLibrary = [];
 
 function closeModal () {
-    modalContainer.classList.remove('show');
+    modalContainer.classList.add('hidden');
 }
+
+function createBookCard () {
+    myLibrary.forEach(function(book){
+        let bookElement = document.createElement('div');
+        let bookInfoElement = document.createElement('div');
+        bookElement.classList.add('bookElement');
+        bookInfoElement.classList.add('bookInfoElement');
+
+        bookInfoElement.textContent = "Book name:" + book.bookName +
+                                    " Author: " + book.author +
+                                    "Pages: " + book.pages;
+        bookElement.appendChild(bookInfoElement);
+        libraryContainer.appendChild(bookElement);
+    })
+};
+
 
 myForm.addEventListener('submit', function(e) {
     e.preventDefault();
-    
-    const bookNameInput = myForm.elements['book-name'].value;
-    const authorInput = myForm.elements['author'].value;
-    const pages = myForm.elements['pages'].value;
-    const checkbox = myForm.elements['checkbox'].value;
-    closeModal();
-    myForm.reset();
+    let bookNameInput = myForm.elements['book-name'].value.trim();
+    let authorInput = myForm.elements['author'].value.trim();
+    let pages = myForm.elements['pages'].value.trim();
+    let checkbox = myForm.elements['checkbox'].value.trim();
+    let newBook = {
+        bookName: bookNameInput,
+        author: authorInput,
+        pages: pages,
+        checkbox: checkbox
+    };
 
 
+    if (bookNameInput === '' || authorInput === '' || pages === '') {
+        e.preventDefault();
+        alert('Please fill in all the required fields.')
+    } else {
+        myLibrary.push(newBook);
+        closeModal();
+        myForm.reset();
+        createBookCard();
+    }
+
+   
 });
 
 open.addEventListener('click', function(){
-    modalContainer.classList.add('show');
-    document.body.style.overflow = 'hidden';
+    modalContainer.classList.remove('hidden');
+    document.body.style.overflow = '';
 });
 
+
 closeBtn.addEventListener('click', function (){
-    modalContainer.classList.remove('show');
-    document.body.style.overflow = '';
+    modalContainer.classList.add('hidden');
+    document.body.style.overflow = 'hidden';
 });
 
 
